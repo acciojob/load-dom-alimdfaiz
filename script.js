b70628945473f58906eb854e3b85414da8f5c776
alimdfaiz <144793353+alimdfaiz@users.noreply.github.com>
2023-12-06 16:04:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-//your JS code here. If required.
-let body = document.queryselector("body");
-body.innerText = "DOM load success";
-console.log(body.innerText);
+  function onDOMLoad() {
+            // Get the body element
+            var bodyElement = document.body;
+
+            // Create a new paragraph element
+            var paragraph = document.createElement("p");
+
+            // Set the text content of the paragraph
+            paragraph.textContent = "DOM load success";
+
+            // Append the paragraph to the body
+            bodyElement.appendChild(paragraph);
+        }
+document.addEventListener("DOMContentLoaded", onDOMLoad);
